7885518fe7179e97f6db069480b0c62a3bece97a
alex97 <kis.alex97@yandex.ru>
2017-05-02 23:01:03 +0300

    Incupsulate run application from execute programm

--- src/main/java/com/alex/chatclient/AppInitializer.java
+++ src/main/java/com/alex/chatclient/AppInitializer.java
@@ -1,40 +1,10 @@
 package com.alex.chatclient;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
 
 public class AppInitializer {
 
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("0.0.0.0", 5003);
-
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-        BufferedReader userIn = new BufferedReader(new InputStreamReader(System.in));
-
-        String input;
-        while ((input = userIn.readLine()) != null) {
-
-            out.println(input);
-
-            // Exit condition
-            if (input.equalsIgnoreCase("exit")) {
-
-                // Inform user
-                String message = "You leave the chat";
-                System.out.println(message);
-
-                break;
-            }
-
-            System.out.println(in.readLine());
-        }
-
-        in.close();
-        out.close();
-        socket.close();
+        ChatClient.run();
     }
 }
