8ab2adde9c506356cfda23ea96f3c2947ee38427
AlexandrKiselev <kis.alex97@gmail.com>
2017-05-14 12:58:55 +0300

    Добавлен заголовок в главном окне.

--- src/main/java/com/alex/chatclient/AppInitializer.java
+++ src/main/java/com/alex/chatclient/AppInitializer.java
@@ -49,6 +49,7 @@ public class AppInitializer extends Application {
             }
         });
 
+        primaryStage.setTitle("Alexis Chat");
         primaryStage.setScene(scene);
 
         primaryStage.show();
